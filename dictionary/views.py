0991082ea28b0f837e0c4b537e21bd6c09c4187f
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-11-25 01:53:38 +0530

    Added pronunciations

--- dictionary/views.py
+++ dictionary/views.py
@@ -18,8 +18,25 @@ def search(request):
 
             if data and isinstance(data[0], dict):  # Ensure valid response
                 meanings = []
+                pronunciations = []
+                audios = []
+
                 for entry in data:
                     if "shortdef" in entry:
+                        pronunciation = None
+                        audio = None
+
+                        # Check for pronunciation
+                        if "prs" in entry["hwi"]:
+                            pronunciation = entry["hwi"]["prs"][0].get("mw")
+                            pronunciations.append(pronunciation)
+
+                            # Check for audio
+                            if "sound" in entry["hwi"]["prs"][0]:
+                                audio_file = entry["hwi"]["prs"][0]["sound"]["audio"]
+                                audio = f"https://media.merriam-webster.com/audio/prons/en/us/mp3/{audio_file[0]}/{audio_file}.mp3"
+                                audios.append(audio)
+
                         meanings.append(
                             {
                                 "headword": entry["hwi"]["hw"],  # The word itself
@@ -27,13 +44,22 @@ def search(request):
                                     "fl", "Unknown"
                                 ),  # Part of speech
                                 "definitions": entry["shortdef"],  # List of definitions
+                                "pronunciation": pronunciation,  # Modern IPA pronunciation
+                                "audio": audio,  # Audio URL (if available)
                             }
                         )
 
                 return render(
                     request,
                     "dictionary/results.html",
-                    {"word": word, "meanings": meanings},
+                    {
+                        "word": word,
+                        "meanings": meanings,
+                        "pronunciation": (
+                            pronunciations[0] if (len(pronunciations) == 1) else None
+                        ),
+                        "audio": audios[0] if (len(audios) == 1) else None,
+                    },
                 )
             else:
                 error = "No results found for this word."
