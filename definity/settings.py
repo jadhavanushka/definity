720ea05259ac66f54169d586a479aa68f239b8d0
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-11-25 02:34:29 +0530

    deploy on vercel

--- definity/settings.py
+++ definity/settings.py
@@ -10,14 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from dotenv import load_dotenv
-import os
 from pathlib import Path
+import environ
 
-load_dotenv()  # Load environment variables from .env file
+env = environ.Env()
+environ.Env.read_env()  # Reads from the .env file
 
 # access API key
-DICTIONARY_API_KEY = os.getenv('DICTIONARY_API_KEY')
+DICTIONARY_API_KEY = env('DICTIONARY_API_KEY')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m!&=&bl)umonvi9ajnh=(s88cq)u=*^a_!4r!v7!2w(gkay7$('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'definity.wsgi.application'
+WSGI_APPLICATION = 'definity.wsgi.app'
 
 
 # Database
