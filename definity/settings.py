7c493a9638ef11220bc5742eb149b35c4cfdff6e
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-11-26 00:40:01 +0530

    deploy on vercel

--- definity/settings.py
+++ definity/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
