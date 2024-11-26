d6c7c58ffc1d13a4e91cdef6fbac4e67d8ba818d
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-11-27 01:33:13 +0530

    updated html templates
    1. created base template
    2. added font for heading
    3. added icon

--- definity/settings.py
+++ definity/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
 import environ
+from pathlib import Path
 
 env = environ.Env()
 environ.Env.read_env()  # Reads from the .env file
@@ -124,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
